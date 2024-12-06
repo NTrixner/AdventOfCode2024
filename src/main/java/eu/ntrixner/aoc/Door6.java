c6c61a9d7bca1925015100715f045ae47f73382f
ext.ntrixner <nikolaus.trixner@cloudflight.io>
2024-12-06 15:05:58 +0100

    Fix

--- src/main/java/eu/ntrixner/aoc/Door6.java
+++ src/main/java/eu/ntrixner/aoc/Door6.java
@@ -3,11 +3,10 @@ package eu.ntrixner.aoc;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Door6 {
@@ -19,9 +18,9 @@ public class Door6 {
         char[][] map;
         public int x, y, height, width, dir;
         public boolean outside;
-        public List<Pair<Integer, Point>> history;
+        public List<PointDir> history;
 
-        public Guard(char[][] map, int x, int y, int dir){
+        public Guard(char[][] map, int x, int y, int dir) {
             this.map = Utils.deepCopy(map);
             this.height = map.length;
             this.width = map[0].length;
@@ -30,29 +29,55 @@ public class Door6 {
             this.dir = dir;
             this.outside = false;
             this.history = new ArrayList<>();
-            history.add(Pair.of(dir, new Point(x, y)));
+            history.add(PointDir.of(x, y, dir));
         }
 
-        public void step()
-        {
+        public void step() {
             int newX = x + dirs.get(dir).x;
             int newY = y + dirs.get(dir).y;
-            if(newX < 0 || newX >= width || newY < 0 || newY >= height) {
+            if (newX < 0 || newX >= width || newY < 0 || newY >= height) {
                 outside = true;
                 return;
             }
-            while(map[newY][newX] == '#'){
+            while (map[newY][newX] == '#') {
                 dir = (dir + 1) % 4;
                 newX = x + dirs.get(dir).x;
                 newY = y + dirs.get(dir).y;
             }
             x = newX;
             y = newY;
-            history.add(Pair.of(dir, new Point(x, y)));
+            history.add(PointDir.of(x, y, dir));
         }
 
         public boolean hasLooped() {
-            return history.size() > 1 && history.subList(1, history.size() - 1).contains(Pair.of(dir, new Point(x, y)));
+            return history.size() > 1 && history.subList(1, history.size() - 1).contains(PointDir.of(x, y, dir));
+        }
+    }
+
+    public static class PointDir {
+        public int x, y, dir;
+
+        public PointDir(int x, int y, int dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public static PointDir of(int x, int y, int dir) {
+            return new PointDir(x, y, dir);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PointDir pointDir = (PointDir) o;
+            return x == pointDir.x && y == pointDir.y && dir == pointDir.dir;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y, dir);
         }
     }
 
@@ -79,28 +104,32 @@ public class Door6 {
 
         Guard g = new Guard(map, cx, cy, dirI);
         Set<Point> obstacles = new HashSet<>();
-        while(!g.outside) {
-            int oldX = g.x;
-            int oldY = g.y;
-            int oldDir = g.dir;
+        Set<Point> coveredTiles = new HashSet<>();
+        Set<Point> ignoredPoints = new HashSet<>();
+        coveredTiles.add(new Point(g.x, g.y));
+        ignoredPoints.add(new Point(g.x, g.y));
 
+        while (!g.outside) {
+            PointDir oldP = PointDir.of(g.x, g.y, g.dir);
             g.step();
-
-            if(wouldLoop(map, g.x, g.y, oldX, oldY, oldDir))
+            coveredTiles.add(new Point(g.x, g.y));
+            if (!ignoredPoints.contains(new Point(g.x, g.y)) && wouldLoop(map, g.x, g.y, oldP.x, oldP.y, oldP.dir) )
                 obstacles.add(new Point(g.x, g.y));
+            else
+                ignoredPoints.add(new Point(g.x, g.y));
         }
 
         System.out.println("Obstacles: " + obstacles.size());
-        System.out.println("CoveredTiles: " + (g.history.stream().map(Pair::getRight).collect(Collectors.toSet()).size()));
+        System.out.println("CoveredTiles: " + coveredTiles.size());
         System.out.println("Checks took " + (System.currentTimeMillis() - ms) + "ms");
     }
 
     public static boolean wouldLoop(char[][] map, int obstacleX, int obstacleY, int startX, int startY, int startDir) {
         Guard g = new Guard(map, startX, startY, startDir);
         g.map[obstacleY][obstacleX] = '#';
-        while(!g.outside) {
+        while (!g.outside) {
             g.step();
-            if(g.hasLooped())
+            if (!g.outside && g.hasLooped())
                 return true;
         }
         return false;
